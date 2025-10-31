fbf245e4911b7549ec08be675e4b3af53810b5a5
Hue Nguyen <huenguyen.tk@gmail.com>
2025-10-31 20:18:30 +0700

    cap nhat phan quyen

--- backend/routes/project.js
+++ backend/routes/project.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const projectController = require('../controllers/projectController');
+const auth = require('../middlewares/auth');
 
-router.post('/', projectController.createProject);
-router.get('/', projectController.getProjects);
-router.get('/:id', projectController.getProjectById);
-router.put('/:id', projectController.updateProject);
-router.delete('/:id', projectController.deleteProject);
+// Read operations: require login
+router.get('/', auth(), projectController.getProjects);
+router.get('/:id', auth(), projectController.getProjectById);
+
+// Write operations: only admin or manager (quanly)
+router.post('/', auth(['admin', 'quanly']), projectController.createProject);
+router.put('/:id', auth(['admin', 'quanly']), projectController.updateProject);
+router.delete('/:id', auth(['admin', 'quanly']), projectController.deleteProject);
 
 module.exports = router; 
